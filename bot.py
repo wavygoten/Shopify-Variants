4539224af33b96d7957a6096d671d8411e5d8793
Goten <64665142+wavygoten@users.noreply.github.com>
2020-09-30 15:01:02 -0500

    fixed price error on oneness

--- bot.py
+++ bot.py
@@ -18,10 +18,13 @@ async def variants(ctx, args):
         # send request to shopify site and parses html content with lxml
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'lxml')
+        
         #####
         
         # # if proxies are being used: put proxy between single quotes and remove curly braces.
         
+
+
         # proxy = '{INSERT-PROXY-HERE}'.split(":")
         # ip, port, user, passw = proxy[0], proxy[1], proxy[2], proxy[3]
         # proxyinuse = { "http": "http://{}:{}@{}:{}".format(user, passw, ip, port),
@@ -30,6 +33,7 @@ async def variants(ctx, args):
         # # then after url in requests, put proxies=proxy, E.G. -- r = requests.get(url,proxies=proxyinuse)
         
         #####
+        
         # keyword filter to find var meta and access the keys inside.
         pattern = re.compile(r"var meta = (.*?);")
         scripts = soup.find("script", text=pattern, attrs=None).string
@@ -42,8 +46,17 @@ async def variants(ctx, args):
         img = soup.find('meta', ({ 'property' : 'og:image'})).get('content')
         
         # search for price
-        price = soup.find('meta',({ 'property' : 'og:price:amount'})).get('content')
-            
+        price = soup.find('meta',({ 'property' : 'og:price:amount'}))
+
+        if price:
+            price = price.get('content')
+        else:
+            price = soup.find('meta',({ 'property' : 'product:price:amount'})).get('content')
+        
+        # keyword filter to find img and access it.
+        # img = soup.find('meta', {'itemprop' : 'image'}).get('content') <- BDGA
+        
+
 
         # convert to json and finds the tuple "product" with the "variants" key inside
         products = json.loads(search.groups()[0]).get('product')['variants']
@@ -60,16 +73,15 @@ async def variants(ctx, args):
         for variants in products:
             id = variants['id']
             lvar += "\n" + str(id)
-        
-        
+         
         value2 ='```' + f"{lvar}" + '```'
-
-
+      
+    
         embed = discord.Embed(title=title,url=url, color=0xf09719)
         embed.set_thumbnail(url=img)
         embed.add_field(name="Sizes", value=value1, inline=True)
         embed.add_field(name="Variants", value=value2, inline=True)
-        embed.add_field(name="Price", value="$" + price, inline=False)
+        embed.add_field(name="Price", value="${}".format(price), inline=False)
         embed.set_footer(icon_url = f"{ctx.guild.icon_url}", text=f"{ctx.message.guild.name} - {ctx.message.author}")
         await ctx.send(embed=embed)
     except Exception as e:
